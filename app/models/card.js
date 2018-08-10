3b39e1f278c799f999884651bb1dfd35496c43af
Karine Delvare <kdelvare@gmail.com>
2018-08-10 07:05:08 +0200

    Show cards as images

---
+++ app/models/card.js
@@ -0,0 +1,21 @@
+import DS from 'ember-data';
+import { computed } from '@ember/object';
+
+export default DS.Model.extend({
+	hs_card_id: DS.attr('string'),
+	name: DS.attr('string'),
+	name_fr: DS.attr('string'),
+	cardtext: DS.attr('string'),
+	cardtext_fr: DS.attr('string'),
+	cost: DS.attr('number'),
+	health: DS.attr('number'),
+	atk: DS.attr('number'),
+
+	image: computed('hs_card_id', function() {
+		const LOCALE = 'frFR';
+		const RESOLUTION = '256x';
+		const CARD_ID = this.get('hs_card_id');
+		const EXT = 'png';
+		return `https://art.hearthstonejson.com/v1/render/latest/${LOCALE}/${RESOLUTION}/${CARD_ID}.${EXT}`;
+	})
+});
