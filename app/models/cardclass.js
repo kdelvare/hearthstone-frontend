4b4dd57dcd791948577f7f00211456d381663f71
Karine Delvare <kdelvare@gmail.com>
2018-08-10 20:48:52 +0200

    Get collectible cardclasses

---
+++ app/models/cardclass.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	name: DS.attr('string'),
+	name_fr: DS.attr('string'),
+	collectible: DS.attr('boolean')
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
 	model() {
-		return this.store.findAll('card');
+		return RSVP.hash({
+			cardclasses: this.store.query('cardclass', { collectible: true }),
+			cards: this.store.findAll('card')
+		});
 	}
 });
