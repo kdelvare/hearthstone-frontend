257475159ea2b090666bbd1aece5ceadb04c698c
Karine Delvare <kdelvare@gmail.com>
2018-08-12 20:29:38 +0200

    Update collections

--- app/controllers/user/collection.js
+++ app/controllers/user/collection.js
@@ -14,6 +14,45 @@ export default Controller.extend({
 			} else {
 				this.set(name, value);
 			}
-		}
+		},
+
+		addToCollection(card) {
+			const user = this.get('model.user');
+			card.get('collections').then((collections) => {
+				const userCollections = collections.filterBy('user_id', user.id_int);
+				if (userCollections.length) {
+					const userCollection = userCollections[0];
+					userCollection.incrementProperty('number');
+					userCollection.save();
+				} else {
+					const userCollection = this.get('store').createRecord('collection', {
+						card_id: card.id,
+						user_id: user.id,
+						number: 1
+					});
+					collections.pushObject(userCollection);
+					userCollection.save();/*.then(() => {
+						card.save();
+					})*/
+				}
+			})
+		},
+
+		removeFromCollection(card) {
+			const user = this.get('model.user');
+			card.get('collections').then((collections) => {
+				const userCollections = collections.filterBy('user_id', user.id_int);
+				if (userCollections.length) {
+					const userCollection = userCollections[0];
+					const number = userCollection.number;
+					if (number > 1) {
+						userCollection.decrementProperty('number');
+					} else {
+						userCollection.deleteRecord();
+					}
+					userCollection.save();
+				}
+			});
+		},
 	}
 });
--- app/models/collection.js
+++ app/models/collection.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	card_id: DS.attr('number'),
 	user_id: DS.attr('number'),
 	number: DS.attr('number')
 });
