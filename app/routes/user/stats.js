07a74607698b417801d50ac17d9b87a1b5f6ad57
Karine Delvare <kdelvare@gmail.com>
2018-09-29 06:52:54 +0200

    User stats: route cleanup

--- app/routes/user/stats.js
+++ app/routes/user/stats.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
-import { assign } from '@ember/polyfills';
 
 export default Route.extend({
-	model(params) {
+	model() {
 		const user = this.modelFor('user');
 		return RSVP.hash({
 			cardsets: this.store.query('cardset', { filter: { collectible: true } }),
