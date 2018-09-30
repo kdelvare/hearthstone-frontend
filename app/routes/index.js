5ea84d4fe80ee36266282bb03151753100fb0a3c
Karine Delvare <kdelvare@gmail.com>
2018-09-30 10:30:34 +0200

    User creation with collection initialization

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,13 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 	model() {
 		return this.store.findAll('user');
+	},
+
+	setupController(controller, model) {
+		this._super(controller, model);
+		if (!controller.get('newUser')) {
+			controller.set('newUser', this.get('store').createRecord('user'));
+		}
+		controller.set('initCollection', 'empty');
 	}
 });
