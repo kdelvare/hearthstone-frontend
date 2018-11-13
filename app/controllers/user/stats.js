c89cbc455465e1103e9b9aadd2c2e2ac10f486b6
Karine Delvare <kdelvare@gmail.com>
2018-11-13 06:21:08 +0100

    User stats: simpler and faster

--- app/controllers/user/stats.js
+++ app/controllers/user/stats.js
@@ -16,14 +16,13 @@ export default Controller.extend({
 
 	completion: computed('model.{stat.completion,cardsets,cardclasses,rarities}', function() {
 		const completion = this.get('model.stat.completion');
-		const cardsets = this.get('model.cardsets');
+		const cardsets = this.get('standard') ? this.get('model.cardsets').filterBy('standard', true) : this.get('model.cardsets');
 		const cardclasses = this.get('model.cardclasses');
 		const rarities = this.get('model.rarities');
 
 		let data = completion;
 		let subdata, common, rare;
 		data.rate = 100 * data.owned / data.total;
-		data.rate_std = 100 * data.owned_std / data.total_std;
 		cardsets.forEach(cardset => {
 			data = completion.cardsets[cardset.id];
 			data.rate = 100 * data.owned / data.total;
@@ -52,17 +51,14 @@ export default Controller.extend({
 		cardclasses.forEach(cardclass => {
 			data = completion.cardclasses[cardclass.id];
 			data.rate = 100 * data.owned / data.total;
-			data.rate_std = 100 * data.owned_std / data.total_std;
 			rarities.forEach(rarity => {
 				subdata = data.rarities[rarity.id];
 				subdata.rate = 100 * subdata.owned / subdata.total;
-				subdata.rate_std = 100 * subdata.owned_std / subdata.total_std;
 			});
 		});
 		rarities.forEach(rarity => {
 			data = completion.rarities[rarity.id];
 			data.rate = 100 * data.owned / data.total;
-			data.rate_std = 100 * data.owned_std / data.total_std;
 		});
 
 		return completion;
@@ -70,14 +66,13 @@ export default Controller.extend({
 
 	total: computed('model.{stat.total,cardsets,cardclasses,rarities}', function() {
 		const total = this.get('model.stat.total');
-		const cardsets = this.get('model.cardsets');
+		const cardsets = this.get('standard') ? this.get('model.cardsets').filterBy('standard', true) : this.get('model.cardsets');
 		const cardclasses = this.get('model.cardclasses');
 		const rarities = this.get('model.rarities');
 
 		let data = total;
 		let subdata;
 		data.rate = 100 * data.owned / data.total;
-		data.rate_std = 100 * data.owned_std / data.total_std;
 		cardsets.forEach(cardset => {
 			data = total.cardsets[cardset.id];
 			data.rate = 100 * data.owned / data.total;
@@ -89,17 +84,14 @@ export default Controller.extend({
 		cardclasses.forEach(cardclass => {
 			data = total.cardclasses[cardclass.id];
 			data.rate = 100 * data.owned / data.total;
-			data.rate_std = 100 * data.owned_std / data.total_std;
 			rarities.forEach(rarity => {
 				subdata = data.rarities[rarity.id];
 				subdata.rate = 100 * subdata.owned / subdata.total;
-				subdata.rate_std = 100 * subdata.owned_std / subdata.total_std;
 			});
 		});
 		rarities.forEach(rarity => {
 			data = total.rarities[rarity.id];
 			data.rate = 100 * data.owned / data.total;
-			data.rate_std = 100 * data.owned_std / data.total_std;
 		});
 
 		return total;
