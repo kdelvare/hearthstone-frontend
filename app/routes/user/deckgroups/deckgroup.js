53128e5fabaab2c356be3c48cd673231093b069b
Karine Delvare <kdelvare@gmail.com>
2018-11-24 16:51:13 +0100

    Fix user deckgroup link

--- app/routes/user/deckgroups/deckgroup.js
+++ app/routes/user/deckgroups/deckgroup.js
@@ -5,7 +5,7 @@ export default Route.extend({
 	model(params) {
 		const user = this.modelFor('user');
 		return RSVP.hash({
-			deckgroup: this.store.findRecord('deckgroup', params.id, {
+			deckgroup: this.store.findRecord('deckgroup', params.deckgroup_id, {
 				include: 'cardset,decks,decks.cardclass,decks.deckcards,decks.deckcards.card,decks.deckcards.card.rarity,decks.deckcards.card.collections,decks.deckcards.card.collections.user,decks.wanteddecks.user',
 				fields: { cardsets: 'name_fr', decks: 'name,url,cardclass,deckcards,wanteddecks', deckcards: 'number,card', collections: 'number,user' }
 			}),
