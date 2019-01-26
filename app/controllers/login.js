cb2d7566b9e1498120728d1b884ff38e0d7e9295
Karine Delvare <kdelvare@gmail.com>
2019-01-26 07:24:15 +0100

    Authentication: wait for currentUser load before redirecting

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,12 +3,15 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
 	session: service(),
+	currentUser: service(),
 
 	actions: {
 		authenticate() {
 			const { login, password } = this.getProperties('login', 'password');
 			this.get('session').authenticate('authenticator:oauth2', login, password).then(() => {
-				this.transitionToRoute('user');
+				this.get('currentUser').load().then(() => {
+					this.transitionToRoute('user.collection');
+				})
 			}).catch((reason) => {
 				this.set('errorMessage', reason.error || reason);
 			});
