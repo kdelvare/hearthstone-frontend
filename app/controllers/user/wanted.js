77bf3c5af53bd817e297fcb1ef01cce99ef5aef3
Karine Delvare <kdelvare@gmail.com>
2019-05-26 21:01:35 +0200

    User wanted: count only 1 per card

--- app/controllers/user/wanted.js
+++ app/controllers/user/wanted.js
@@ -9,14 +9,14 @@ export default Controller.extend({
 			index = card_ids.indexOf(card_id);
 			if (index > -1) {
 				group = groups[index];
-				group.total += wantedcard.number;
+				group.total += 1;//wantedcard.number;
 				group.wantedcards.push(wantedcard);
 				if (group.max < wantedcard.number) group.max = wantedcard.number;
 			} else {
 				card_ids.push(card_id);
 				groups.push({
 					card: wantedcard.card,
-					total: wantedcard.number,
+					total: 1,//wantedcard.number,
 					max: wantedcard.number,
 					wantedcards: [wantedcard]
 				});
